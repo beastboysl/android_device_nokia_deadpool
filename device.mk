b70c058f481a58c4735170b9e10462b326859823
beastboysl <pabasaradulanjana8@gmail.com>
2026-09-17 18:52:35 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -16,13 +16,10 @@ AB_OTA_POSTINSTALL_CONFIG += \
 # Boot control HAL
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-impl.recovery \
-    android.hardware.boot@1.0-service \
-    android.hardware.boot@1.0-service.recovery
+    android.hardware.boot@1.0-service
 
 PRODUCT_PACKAGES += \
     libgptutils \
-    libgptutils.recovery \
     libz \
     libcutils
 
